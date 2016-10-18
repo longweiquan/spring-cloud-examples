77e58c73c5fd00e1c59d711b125a341b9eae9f6f
Weiquan LONG <weiquan.long@adotmob.com>
2016-10-18 13:56:54 +0200

    initilize project with spring initializr

---
+++ src/main/java/com/lwq/SpringCloudExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.lwq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudExamplesApplication.class, args);
+	}
+}
